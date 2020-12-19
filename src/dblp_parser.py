e5b93be5c5e724c0f68b14d712938e17b77ee628
Antonio Ferrara <antonioferrara94@icloud.com>
2020-12-19 10:53:28 +0100

    Update dblp_parser.py

--- src/dblp_parser.py
+++ src/dblp_parser.py
@@ -238,7 +238,7 @@ def main():
     except IOError:
         log_msg("ERROR: Failed to load file \"{}\". Please check your XML and DTD files.".format(dblp_path))
         exit()
-    parse_filtered(dblp_path)
+    parse_filtered(dblp_path, save_path)
 
 
 if __name__ == '__main__':
